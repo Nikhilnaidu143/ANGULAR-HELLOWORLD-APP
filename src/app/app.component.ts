ea4ee040ddf97a9a831e86a147f846389681bdf6
saitarun135 <nikhilnaidu473@gmail.com>
2022-02-14 14:53:56 +0530

    UC-5:- Validated user name with regex pattern.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent {
   imageUrl = "../assets/bridgelabz-logo.png";
   siteUrl = "https://www.bridgelabz.com/";
   userName : string;
+  nameError : string;
 
   /** One way data binding. */
   ngOnInit() : void {
@@ -20,5 +21,20 @@ export class AppComponent {
   onClick($event) {
     console.log("clicked logo!" , $event);
     window.open(this.siteUrl);
+  }
+
+  /** Validating user name with regex pattern. */
+  onInput($event) {
+    console.log("change event occured!" , $event.data);
+    const nameRegex = /^[A-Z]{1}[A-Za-z\\s]{2,}/;
+    if(this.userName === ""){
+      return this.nameError = " ";
+    }
+    else if(nameRegex.test(this.userName)) {
+      return this.nameError = " "; 
+    }
+    else {
+      return this.nameError = "Invalid name...!";
+    }
   }
 }
