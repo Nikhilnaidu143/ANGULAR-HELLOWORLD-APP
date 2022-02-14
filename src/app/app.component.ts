a86f7d0c8b83d82f645d0120aae602c17e44060a
saitarun135 <nikhilnaidu473@gmail.com>
2022-02-14 14:24:10 +0530

    UC-2:- Used property binding to display BridgeLabz logo.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'HelloWorld';
+  imageUrl = "../assets/bridgelabz-logo.png";
 
   /** One way data binding. */
   ngOnInit() : void {
