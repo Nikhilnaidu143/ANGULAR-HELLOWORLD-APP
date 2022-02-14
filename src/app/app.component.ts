772ea0494ef4de3be149299de1ccd074a03eebac
saitarun135 <nikhilnaidu473@gmail.com>
2022-02-14 14:15:02 +0530

    UC-1.1:- Used one way data binding to display given string.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'HelloWorld';
+
+  /** One way data binding. */
+  ngOnInit() : void {
+    this.title = "Hello from BridgeLabz";
+  }
 }
