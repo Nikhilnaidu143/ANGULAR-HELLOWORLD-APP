5b03dd76f9a6d55949105d7028d779ebccbb15c1
saitarun135 <nikhilnaidu473@gmail.com>
2022-02-14 14:32:26 +0530

    UC-3:- Used event binding to launch BridgeLabz site when clicked on logo.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,16 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'HelloWorld';
   imageUrl = "../assets/bridgelabz-logo.png";
+  siteUrl = "https://www.bridgelabz.com/";
 
   /** One way data binding. */
   ngOnInit() : void {
     this.title = "Hello from BridgeLabz";
+  }
+
+  /** Event binding */
+  onClick($event) {
+    console.log("clicked logo!" , $event);
+    window.open(this.siteUrl);
   }
 }
